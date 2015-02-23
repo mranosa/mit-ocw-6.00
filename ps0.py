d15cd6406d83d00a363d3af53ec09c9bddcc8e97
Mark Kenneth Ranosa <mark.ranosa@gmail.com>
2015-02-23 10:54:22 +0800

    Problem Set 0

---
+++ ps0.py
@@ -0,0 +1,8 @@
+# Problem Set 0
+# Name: Ken
+# Collaborators: 
+# Time Spent: >5:00
+
+birthDate = raw_input('Enter your date of birth:\n')
+lastName= raw_input('Enter your last name:\n')
+print (birthDate + ' ' + lastName)
